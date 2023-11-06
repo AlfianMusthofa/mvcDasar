bd852ea3ae38589da90558ff84b862a2ebd04af8
alfian <alfianmusthofa45264@gmail.com>
2023-11-06 13:08:00 +0700

    assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -5,6 +5,6 @@
         <p class="lead mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores pariatur nulla nobis velit modi fugit soluta voluptas et ipsum eos possimus est architecto laborum, inventore quaerat, ex tempore optio deleniti.</p>
         <hr class="my-4">
         <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        <a class="btn btn-primary btn-lg" href="<?= BASEURL ?>/About/page" role="button">Learn more</a>
     </div>
 </div>
