c3ef5536e12d8bbae284513aa5679b15ceca455b
alfian <alfianmusthofa45264@gmail.com>
2023-11-06 12:58:09 +0700

    assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,10 @@
-<h1>About me</h1>
-<p>Halo nama saya <?= $data['nama'] ?>, saya adalah seorang <?= $data['status'] ?>.</p>
+<div class="container">
+    <div class="jumbotron mt-5">
+        <h1 class="display-4">About me!</h1>
+        <img src="<?= BASEURL; ?>/img/bg.jpg" alt="" width="200px">
+        <p class="lead mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores pariatur nulla nobis velit modi fugit soluta voluptas et ipsum eos possimus est architecto laborum, inventore quaerat, ex tempore optio deleniti.</p>
+        <hr class="my-4">
+        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </div>
+</div>
--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,3 +1,4 @@
+<script src="<?= BASEURL ?>/js/bootstrap.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 
 </html>
